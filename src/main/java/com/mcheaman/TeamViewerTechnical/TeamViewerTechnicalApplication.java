bce77d5c33817e6c20dd55635237f652be64690f
Mason <admin@macpro.mynetworksettings.com>
2024-01-08 16:18:55 -0800

    Initialize Spring Boot Project

---
+++ src/main/java/com/mcheaman/TeamViewerTechnical/TeamViewerTechnicalApplication.java
@@ -0,0 +1,13 @@
+package com.mcheaman.TeamViewerTechnical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamViewerTechnicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamViewerTechnicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcheaman/TeamViewerTechnical/TeamViewerTechnicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcheaman.TeamViewerTechnical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamViewerTechnicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
